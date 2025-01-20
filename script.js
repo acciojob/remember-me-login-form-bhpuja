5f880ac4fa08daf4dc1c720162596b21e5a594fa
bhpuja <vpuja0421@gmail.com>
2025-01-20 16:27:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 //your JS code here. If required.
+
 window.onload = function() {
     // Check if there's saved login information in local storage
     const savedUsername = localStorage.getItem('username');
